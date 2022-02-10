510cfcf7076992da17382dc48a3094b05664ba7f
Benjamin Carter <benj.james.carter@gmail.com>
2022-02-09 20:09:05 -0800

    Added final js functionality and questions

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
+let correctAnswers = 0;
 let userName = capitalizeFirstLetter(prompt('Hello and welcome! Whats your name?'));
 alert(`Welcome ${userName}, Here is a handful of questions about me!`);
 
@@ -24,15 +24,17 @@ if (answerOne === yes || answerOne === y) {
 } else if (answerOne === no || answerOne === n) {
     alert(`You got it ${userName}! No puppy in my life.`);
     // console.log('Nailed it! I\'ve got a wonderful cat!');
+    correctAnswers += 1;
 } else {
     console.log('Please answer with a yes or no');
 }
-
+console.log(correctAnswers);
 // question two
 let answerTwo = prompt('Am I married?').toUpperCase();
 
 if (answerTwo === yes || answerTwo === y) {
     alert(`That is correct ${userName}! I got married in August of 2020.`);
+    correctAnswers += 1;
     // console.log('You got it! I am married.');
 } else if (answerTwo === no || answerTwo === n) {
     alert(`Sorry that is incorrect ${userName}. I got married in August of 2020.`);
@@ -46,6 +48,7 @@ let answerThree = prompt('Am I a musician?').toUpperCase();
 
 if (answerThree === yes || answerThree === y) {
     alert(`I think the term is a little generous for what I do, but you are correct ${userName}.`);
+    correctAnswers += 1;
     // console.log('Correct! Ben poorly plays guitar in a terrible punk band!');
 } else if (answerThree === no || answerThree === n) {
     alert(`You get partial credit ${userName}. I dont think of myself as one, but I do play guitar.`);
@@ -59,6 +62,7 @@ let answerFour = prompt('Am I a Washington state native?').toUpperCase();
 
 if (answerFour === yes || answerFour === y) {
     alert(`Nailed it! ${userName}. Born and raised here in the evergreen state!`);
+    correctAnswers += 1;
     // console.log('Correct answer! I was born in Everett.');
 } else if (answerFour === no || answerFour === n) {
     alert(`Ah, sorry ${userName}, I am a Washington state native.`);
@@ -72,6 +76,7 @@ let answerFive = prompt('Am I ambidextrous?').toUpperCase();
 
 if (answerFive === yes || answerFive === y) {
     alert(`Thats right ${userName}, I swap between left and right hand depending on the task I am doing.`);
+    correctAnswers += 1;
     // console.log('Correct answer! I am ambidextrous.');
 } else if (answerFive === no || answerFive === n) {
     alert(`Ah darn, wrong answer ${userName}.`);
@@ -86,8 +91,8 @@ let numGuess = prompt(`Okay ${userName} Guess a number between 0 and 10. Whole n
 let correctNum = Math.floor(Math.random() * 10);
 let i = 0;
 
-// While `i` is less than or equal too 6 Loop through the If else statement. which checks if the number is high or low. If the guess is correct, break out of the while loop. 
-while (i <= 6) {
+// While `i` is less than or equal too 5 Loop through the if else statement. which checks if the number is high or low. If the guess is correct, break out of the while loop.
+while (i < 5) {
     i++;
     if (numGuess > correctNum) {
         alert(`Your guess of ${numGuess} Is too high! Guess again.`);
@@ -96,12 +101,49 @@ while (i <= 6) {
         alert(`Your guess of ${numGuess} is too low! Guess again.`);
         numGuess = prompt(`Enter a new guess ${userName}, your last guess of ${numGuess} was too low. `);
     } else {
+        correctAnswers += 1;
         alert(`You got it ${userName}! the correct number is ${correctNum} and it took you ${i} guesses.`);
         break;
     }
 }
 
+// question 7
+// create an array with multiple objects. These objects are the possible correct answers to a question.
+// ask the question and prompt the user for an answer. store the answer and iterate through the array to check for that answer. If the array contains the answer congratulate and exit loop
+// If the array doers not contain the users answer, ++ to a counter and prompt the user to guess again. The user should have 6 guesses before the loop exits with a "sorry you didn't get it within 6 guesses." And display contents of the array to show the correct answers.
+//use a for loop to iterate through the array
 
 
+let myArr = ['BRAZIL', 'THAILAND', 'IRELAND', 'CHINA', 'CANADA', 'MEXICO', 'ICELAND'];
+let answer = prompt('I\'ve been lucky to tavel a lot in my life. Name one country you think I have traveled too!').toUpperCase();
 
+let turn = 0;
+let index = 0;
+let attempts = 6;
+breaklabel:
+for (turn = 0; turn < attempts; turn++) {
+    do {
+        let isTrue = false;
+        for (index = 0; index < myArr.length; index++) {
+            isTrue = (answer === myArr[index]);
+            if (isTrue === true) {
+                break;
+            }
+        }
+        if (isTrue) {
+            alert(`You got one! I loved traveling to ${answer}`);
+            answer = true;
+            correctAnswers += 1;
+            break breaklabel;
+        } else if (turn === 5) {
+            alert(`You've run out of turns, the correct answers were ${myArr}`);
+            break breaklabel;
+        } else {
+            alert('Sorry I haven\'t been there.');
+            answer = prompt('Try another country.').toUpperCase();
+            break;
+        }
+    } while (answer !== true);
+}
 
+alert(`Okay, ${userName} you got ${correctAnswers} out of 7 questions correct!`);
