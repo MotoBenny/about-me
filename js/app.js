d113be135373568fd2207b803f338228f76732ab
Benjamin Carter <benj.james.carter@gmail.com>
2022-02-10 22:59:55 -0800

    cleaned up code base, re-formatted spaces.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// ask the user for their name, than as a response to each question use template literals to return a customized answer.
-// prompt to establish userName let. And use
-
 // a JS function to capitalize the first letter of a string. sourced from https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -12,10 +9,7 @@ let userName = capitalizeFirstLetter(prompt('Hello and welcome! Whats your name?
 alert(`Welcome ${userName}, Here is a handful of questions about me!`);
 let correct = 0;
 
-
-
 function questionCat(userName) {
-    //let correct = 0;
     // question one for About me
     let answerOne = prompt('Do I have a puppy?').toUpperCase();
     const yes = 'YES';
@@ -25,23 +19,17 @@ function questionCat(userName) {
 
     if (answerOne === yes || answerOne === y) {
         alert(`I do not ${userName}! I've got a great kitty cat.`);
-        //console.log('You are wrong, I dont have a dog.');
-
     } else if (answerOne === no || answerOne === n) {
         alert(`You got it ${userName}! No puppy in my life.`);
-        // console.log('Nailed it! I\'ve got a wonderful cat!');
         correct += 1;
     } else {
         console.log('Please answer with a yes or no');
     }
     console.log(correct);
-    //return correct;
 }
 
-
 function questionMarried(userName) {
-    //let correct =0
-    // question two 
+    // question two
     let answerTwo = prompt('Am I married?').toUpperCase();
     const yes = 'YES';
     const y = 'Y';
@@ -51,18 +39,13 @@ function questionMarried(userName) {
     if (answerTwo === yes || answerTwo === y) {
         alert(`That is correct ${userName}! I got married in August of 2020.`);
         correct += 1;
-        // console.log('You got it! I am married.');
     } else if (answerTwo === no || answerTwo === n) {
         alert(`Sorry that is incorrect ${userName}. I got married in August of 2020.`);
-        // console.log('Sorry thats wrong. I am married.');
     } else {
         console.log('Please answer with a yes or no');
     }
-    //return correct;
     console.log(correct);
 }
-
-
 
 function questionMusic(userName) {
     // question three
@@ -82,7 +65,6 @@ function questionMusic(userName) {
     } else {
         console.log('Please answer with a yes or no');
     }
-    //return correctAnswers;
     console.log(correct);
 }
 
@@ -97,14 +79,11 @@ function questionWa(userName) {
     if (answerFour === yes || answerFour === y) {
         alert(`Nailed it! ${userName}. Born and raised here in the evergreen state!`);
         correct += 1;
-        // console.log('Correct answer! I was born in Everett.');
     } else if (answerFour === no || answerFour === n) {
         alert(`Ah, sorry ${userName}, I am a Washington state native.`);
-        // console.log('answered incorrectly. I was born in Everett.');
     } else {
         console.log('Please answer with a yes or no');
     }
-    //return correctAnswers;
     console.log(correct);
 }
 
@@ -121,14 +100,11 @@ function questionAmbi(userName) {
     if (answerFive === yes || answerFive === y) {
         alert(`Thats right ${userName}, I swap between left and right hand depending on the task I am doing.`);
         correct += 1;
-        // console.log('Correct answer! I am ambidextrous.');
     } else if (answerFive === no || answerFive === n) {
         alert(`Ah darn, wrong answer ${userName}.`);
-        // console.log('Incorrect answer, I am.');
     } else {
         console.log('Please answer with a yes or no');
     }
-    //return correctAnswers;
     console.log(correct);
 }
 
@@ -139,9 +115,6 @@ function questionNumb(userName) {
     let numGuess = prompt(`Okay ${userName} Guess a number between 0 and 10. Whole numbers only!`);
     let correctNum = Math.floor(Math.random() * 10);
     let i = 0;
-
-    // While `i` is less than or equal too 5 Loop through the if else statement. which checks if the number is high or low. If the guess is correct, break out of the while loop.
-    // I still need to tell the user what the correct number was if they run out of guesses.
 
     while (i < 3) {
         i++;
@@ -158,7 +131,6 @@ function questionNumb(userName) {
         }
     }
     alert(`Sorry ${userName}, You've run out of guesses. The correct number was ${correctNum}`);
-    //return correctAnswers;
     console.log(correct);
 }
 
@@ -196,7 +168,7 @@ function questionTravel(userName) {
                 alert(`You've run out of turns, the correct answers were ${myArr}`);
                 break breaklabel;
             } else {
-                alert('Sorry I haven\'t been there.');
+                alert(`Sorry ${userName} I haven't been there.`);
                 answer = prompt('Try another country.').toUpperCase();
                 break;
             }
@@ -207,8 +179,6 @@ function questionTravel(userName) {
 }
 
 
-
-
 questionCat(userName);
 questionMarried(userName);
 questionMusic(userName);
@@ -217,33 +187,4 @@ questionAmbi(userName);
 questionNumb(userName);
 questionTravel(userName);
 alert(`Okay, ${userName} you got ${correct} out of 7 questions correct!`);
-
-
-
-
-/*
-
-
-let attempts = 4;
-let answer = Math.floor(Math.random() * 10);
-
-while (attempts){
-    attempts--;
-    let userGuess = prompt('Guess a number between 0 and 10');
-    if (+userGuess === answer) {
-    alert('Yay you got the answer correct');
-    break;
-    } else if (+userGuess > answer) {
-    alert('Too high');
-    } else {
-    alert('Too low');
-    }
-
-    if (attempts === 0) {
-    alert(`You've run out of quesses the correct number was ${answer}`);
-    }
-}
-
-*/
-
 
